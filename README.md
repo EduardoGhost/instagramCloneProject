194ca2c30e610f56476648d374788988a3f49ee0
Eduardo Freire <61304055+EduardoGhost@users.noreply.github.com>
2022-12-29 19:57:08 -0300

    perfilScreen

---
+++ README.md
@@ -0,0 +1,2 @@
+# instagramCloneProject
+ 
---
+++ app/src/main/java/com/example/instagramCloneProject/ImageWithText.kt
@@ -0,0 +1,8 @@
+package com.example.instagramCloneProject
+
+import androidx.compose.ui.graphics.painter.Painter
+
+data class ImageWithText(
+    val image: Painter,
+    val text: String
+)
